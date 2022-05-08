15587eec009d84035dfaef3d9481c9e6e9d66e79
Victor Kjelde <201809521@post.au.dk>
2022-05-08 14:51:21 +0200

    test

--- MobileApp/Assets/Scripts/CreateSpace.cs
+++ MobileApp/Assets/Scripts/CreateSpace.cs
@@ -103,7 +103,7 @@ public class CreateSpace : MonoBehaviour
     
     private void DragGestureRecognizerStarted(Gesture<DragGesture> dragGesture)
     {
-        const float acceleration = 0.01f;
+        const float acceleration = 0.2f;
         if (_plane == null)
         {
             return;
